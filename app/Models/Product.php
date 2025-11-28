5977a9ed2d38a9ae6b5c0a153e20182de17b2a3e
Qawee <qaweeds@gmail.com>
2025-11-28 13:56:52 +0200

    fix sales special

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -70,7 +70,7 @@ class Product extends Model
         return $this->hasOne(ProductSpecial::class, 'product_id', 'product_id')
             ->where('priority', 1)
             ->where('customer_group_id', 1)
-            ->whereRaw('current_date() between date_start and date_end');
+            ->whereRaw('now() between date_start and date_end');
     }
 
     public function price()
